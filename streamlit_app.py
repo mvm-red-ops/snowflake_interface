dedeab25d579fb5f108aa8b40670ba0fcdff3c10
taylor <tayloryoung@mvmediasales.com>
2023-01-23 11:22:56 -0500

    sf update query

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,17 +9,27 @@ st.write("DB acct:", st.secrets["db_acct"])
 
 
 
-# Gets the version
-ctx = snowflake.connector.connect(
-    user=st.secrets["db_user"],
-    password=st.secrets["db_pw"],
-    account=st.secrets["db_acct"]
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return snowflake.connector.connect(
+        **st.secrets["snowflake"], client_session_keep_alive=True
     )
-cs = ctx.cursor()
-try:
-    cs.execute("SELECT current_version()")
-    one_row = cs.fetchone()
-    print(one_row[0])
-finally:
-    cs.close()
-ctx.close()
+
+conn = init_connection()
+
+
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from payments_master;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
